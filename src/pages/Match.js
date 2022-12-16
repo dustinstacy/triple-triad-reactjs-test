1ac29fffb00733fad71e25ff45d5881e0ce85fe3
Dustin Stacy <dustin.stacy@outlook.com>
2022-12-16 11:37:11 -0500

    further Match.js refactor

--- src/pages/Match.js
+++ src/pages/Match.js
@@ -2,37 +2,31 @@ import React, { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Button from "../components/Button";
-import { cards } from "../card-data/card-data";
-
-const MAX_VALUE = 10;
+import Card from "../components/Card";
+import { useCardsCtx } from "../context/DemoCardsContext";
 
 // customizable options?
 const width = 3;
-const deckSize = 10;
+const handSize = 5;
 
 const Match = () => {
+  const { p1cards, p2cards } = useCardsCtx();
+
   const [boardArray, setBoardArray] = useState([]);
-  const [redHand, setRedHand] = useState([]);
-  const [blueHand, setBlueHand] = useState([]);
+  const [p1Hand, setP1Hand] = useState([]);
+  const [p2Hand, setP2Hand] = useState([]);
   const [cardBeingDragged, setCardBeingDragged] = useState(null);
   const [cellBeingFilled, setCellBeingFilled] = useState(null);
-  const [isBlueTurn, setIsBlueTurn] = useState(false);
-  const [blueScore, setBlueScore] = useState(5);
-  const [redScore, setRedScore] = useState(5);
+  const [isP1Turn, setisP1Turn] = useState(true);
+  const [p1Score, setP1Score] = useState(5);
+  const [p2Score, setP2Score] = useState(5);
 
-  const table = [...blueHand, ...redHand, ...boardArray];
+  const table = [...p1Hand, ...boardArray, ...p2Hand];
 
-  const randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+  let p1ScoreCounter = 0;
+  let p2ScoreCounter = 0;
 
-  const common = randomIntFromInterval(8, 12);
-  const uncommon = randomIntFromInterval(13, 17);
-  const rare = randomIntFromInterval(18, 22);
-  const epic = randomIntFromInterval(23, 27);
-  const legendary = randomIntFromInterval(28, 32);
-
-  function shuffleCards(cards) {
+  const shuffleCards = useCallback((cards) => {
     let i = cards.length,
       temp,
       rand;
@@ -44,61 +38,48 @@ const Match = () => {
       cards[rand] = temp;
     }
     return cards;
-  }
-
-  function dealHands(blue, red) {
-    for (let card = 0; card < deckSize; card++) {
-      // if even, move to blue deck
-      if (card % 2 === 0) {
-        cards[card].owner = "blue";
-        blue.push(cards[card]);
-        // if odd, move to red deck
-      } else if (card % 2 !== 0) {
-        cards[card].owner = "red";
-        red.push(cards[card]);
-      }
-    }
-  }
-
-  const randomizeValues = useCallback((rarity, max, len = 4) => {
-    let startValues = new Array(len);
-    let sum = 0;
-    do {
-      for (let i = 0; i < len; i++) {
-        startValues[i] = Math.random();
-      }
-      sum = startValues.reduce((acc, val) => acc + val, 0);
-      const scale = (rarity - len) / sum;
-      startValues = startValues.map((val) => Math.min(max, Math.round(val * scale) + 1));
-      sum = startValues.reduce((acc, val) => acc + val, 0);
-    } while (sum - rarity);
-    const values = startValues.map((value) => {
-      if (value === 10) {
-        return "A";
-      }
-      return value;
-    });
-    return values;
   }, []);
 
-  const assignRandomValues = useCallback(
-    (array) => {
-      array.forEach((card) => {
-        if (Object.values(card).includes("common")) {
-          card.values = randomizeValues(common, MAX_VALUE);
-        } else if (Object.values(card).includes("uncommon")) {
-          card.values = randomizeValues(uncommon, MAX_VALUE);
-        } else if (Object.values(card).includes("rare")) {
-          card.values = randomizeValues(rare, MAX_VALUE);
-        } else if (Object.values(card).includes("epic")) {
-          card.values = randomizeValues(epic, MAX_VALUE);
-        } else if (Object.values(card).includes("legendary")) {
-          card.values = randomizeValues(legendary, MAX_VALUE);
-        }
-      });
+  const dealCards = useCallback(
+    (p1, p2) => {
+      for (let card = 0; card < handSize; card++) {
+        p1cards[card].owner = "red";
+        p1.push(p1cards[card]);
+        p2cards[card].owner = "blue";
+        p2.push(p2cards[card]);
+        console.log(p1cards);
+      }
     },
-    [epic, legendary, common, uncommon, rare, randomizeValues]
+    [p1cards, p2cards]
   );
+
+  const createBoard = useCallback(() => {
+    const emptyCellArray = [];
+    for (let i = 0; i < width * width; i++) {
+      const emptyCell = { empty: "true" };
+      emptyCellArray.push(emptyCell);
+    }
+    setBoardArray(emptyCellArray);
+  }, []);
+
+  const createHands = useCallback(() => {
+    const p1HandArray = [];
+    const p2HandArray = [];
+    dealCards(p1HandArray, p2HandArray);
+    setP1Hand(p1HandArray);
+    setP2Hand(p2HandArray);
+  }, [dealCards]);
+
+  const newGame = useCallback(() => {
+    shuffleCards(p1cards);
+    shuffleCards(p2cards);
+    createBoard();
+    createHands();
+  }, [shuffleCards, createBoard, createHands, p1cards, p2cards]);
+
+  useEffect(() => {
+    newGame();
+  }, [newGame]);
 
   const dragStart = (e) => {
     setCardBeingDragged(e.target);
@@ -112,202 +93,148 @@ const Match = () => {
     const cellBeingFilledId = parseInt(cellBeingFilled.getAttribute("data-id"));
     const cardBeingDraggedId = parseInt(cardBeingDragged.getAttribute("data-id"));
 
+    boardArray[cellBeingFilledId].empty = "false";
+    cellBeingFilled.innerHTML = cardBeingDragged.innerHTML;
+
+    if (isP1Turn) {
+      boardArray[cellBeingFilledId] = {
+        ...boardArray[cellBeingFilledId],
+        ...p1Hand[cardBeingDraggedId],
+      };
+    } else if (!isP1Turn) {
+      boardArray[cellBeingFilledId] = {
+        ...boardArray[cellBeingFilledId],
+        ...p2Hand[cardBeingDraggedId],
+      };
+    }
+
+    processBattles(cellBeingFilledId);
+    endTurn(cardBeingDraggedId);
+  };
+
+  const processBattles = (cellBeingFilledId) => {
     const up = boardArray[cellBeingFilledId - width];
     const right = boardArray[cellBeingFilledId + 1];
     const left = boardArray[cellBeingFilledId - 1];
     const down = boardArray[cellBeingFilledId + width];
 
-    let redScoreCounter = 0;
-    let blueScoreCounter = 0;
-
-    boardArray[cellBeingFilledId].empty = "false";
-    cellBeingFilled.innerHTML = cardBeingDragged.innerHTML;
-    cardBeingDragged.innerHTML = "";
-
-    if (isBlueTurn) {
-      boardArray[cellBeingFilledId] = {
-        ...boardArray[cellBeingFilledId],
-        ...blueHand[cardBeingDraggedId],
-      };
-    } else if (!isBlueTurn) {
-      boardArray[cellBeingFilledId] = {
-        ...boardArray[cellBeingFilledId],
-        ...redHand[cardBeingDraggedId],
-      };
-    }
-
     if (cellBeingFilledId !== 0 && cellBeingFilledId !== 1 && cellBeingFilledId !== 2 && up.empty === "false") {
-      if (up.values[3] > boardArray[cellBeingFilledId].values[0]) {
-        console.log("you lose");
-      } else if (up.values[3] < boardArray[cellBeingFilledId].values[0]) {
-        console.log("you win");
+      if (up.values[3] < boardArray[cellBeingFilledId].values[0]) {
         up.owner = boardArray[cellBeingFilledId].owner;
-      } else if (up.values[3] === boardArray[cellBeingFilledId].values[0]) {
-        console.log("draw");
       }
     }
     if (cellBeingFilledId !== 0 && cellBeingFilledId !== 3 && cellBeingFilledId !== 6 && left.empty === "false") {
-      if (left.values[2] > boardArray[cellBeingFilledId].values[1]) {
-        console.log("you lose");
-      } else if (left.values[2] < boardArray[cellBeingFilledId].values[1]) {
-        console.log("you win");
+      if (left.values[2] < boardArray[cellBeingFilledId].values[1]) {
         left.owner = boardArray[cellBeingFilledId].owner;
-      } else if (left.values[2] === boardArray[cellBeingFilledId].values[1]) {
-        console.log("draw");
       }
     }
     if (cellBeingFilledId !== 2 && cellBeingFilledId !== 5 && cellBeingFilledId !== 8 && right.empty === "false") {
-      if (right.values[1] > boardArray[cellBeingFilledId].values[2]) {
-        console.log("you lose");
-      } else if (right.values[1] < boardArray[cellBeingFilledId].values[2]) {
-        console.log("you win");
+      if (right.values[1] < boardArray[cellBeingFilledId].values[2]) {
         right.owner = boardArray[cellBeingFilledId].owner;
-      } else if (right.values[1] === boardArray[cellBeingFilledId].values[2]) {
-        console.log("draw");
       }
     }
     if (cellBeingFilledId !== 6 && cellBeingFilledId !== 7 && cellBeingFilledId !== 8 && down.empty === "false") {
-      if (down.values[0] > boardArray[cellBeingFilledId].values[3]) {
-        console.log("you lose");
-      } else if (down.values[0] < boardArray[cellBeingFilledId].values[3]) {
-        console.log("you win");
+      if (down.values[0] < boardArray[cellBeingFilledId].values[3]) {
         down.owner = boardArray[cellBeingFilledId].owner;
-      } else if (down.values[0] === boardArray[cellBeingFilledId].values[3]) {
-        console.log("draw");
       }
     }
+  };
 
-    if (isBlueTurn) {
-      blueHand.splice([cardBeingDraggedId], 1);
-    } else if (!isBlueTurn) {
-      redHand.splice([cardBeingDraggedId], 1);
+  const endTurn = (cardBeingDraggedId) => {
+    if (isP1Turn) {
+      p1Hand.splice([cardBeingDraggedId], 1);
+    } else if (!isP1Turn) {
+      p2Hand.splice([cardBeingDraggedId], 1);
     }
 
     setCellBeingFilled(null);
     setCardBeingDragged(null);
-    setIsBlueTurn((current) => !current);
+    setisP1Turn((current) => !current);
     setBoardArray(boardArray);
-    console.log(table);
 
     table.forEach((card) => {
       if (card.owner === "blue") {
-        blueScoreCounter++;
+        p2ScoreCounter++;
       }
       if (card.owner === "red") {
-        redScoreCounter++;
+        p1ScoreCounter++;
       }
     });
 
-    setBlueScore(blueScoreCounter);
-    setRedScore(redScoreCounter);
+    setP2Score(p2ScoreCounter);
+    setP1Score(p1ScoreCounter);
   };
-
-  const createBoard = () => {
-    const emptyCellArray = [];
-    for (let i = 0; i < width * width; i++) {
-      const emptyCell = { empty: "true" };
-      emptyCellArray.push(emptyCell);
-    }
-    setBoardArray(emptyCellArray);
-  };
-
-  const createHands = () => {
-    const redHandArray = [];
-    const blueHandArray = [];
-    shuffleCards(cards);
-    dealHands(blueHandArray, redHandArray);
-    assignRandomValues(redHandArray);
-    assignRandomValues(blueHandArray);
-    setRedHand(redHandArray);
-    setBlueHand(blueHandArray);
-  };
-
-  useEffect(() => {
-    createBoard();
-    createHands();
-  }, []);
 
   return (
-    <MatchLayout>
-      <Link to="../Setup">
-        <Button label={"<"} />
-      </Link>
+    <DevLayout>
+      <ButtonBar>
+        <Link to="/">
+          <Button label="Main Menu" />
+        </Link>
+        <Link to="/Match">
+          <Button label="Reset" />
+        </Link>
+      </ButtonBar>
       <Table>
-        <TurnmarkerRed>{isBlueTurn ? "" : "-->"}</TurnmarkerRed>
+        <P1TurnMarker>{isP1Turn ? "-->" : ""}</P1TurnMarker>
         <Hand>
-          {redHand.map((card, i) => (
-            <RedCards
-              key={card.number}
+          {p1Hand.map((card, i) => (
+            <P1Cards
+              key={card.id}
               data-id={i}
-              draggable={isBlueTurn ? false : true}
+              draggable={isP1Turn ? true : false}
               onDragStart={dragStart}
               onDragEnd={dragEnd}
             >
-              <Card className="card red" {...card} name={card.name} owner={card.owner}>
-                <CharImage src={`../images/cardImages/card${card.number}.png`} alt={card.name}></CharImage>
-                <Values>
-                  <Up>{card.values[0]}</Up>
-                  <Right>{card.values[1]}</Right>
-                  <Left>{card.values[2]}</Left>
-                  <Down>{card.values[3]}</Down>
-                </Values>
-              </Card>
-            </RedCards>
+              <Card {...card} owner={card.owner}></Card>
+            </P1Cards>
           ))}
         </Hand>
-        <ScoreRed>{redScore}</ScoreRed>
+        <P1Score>{p1Score}</P1Score>
         <Board>
           {boardArray.map((_, i) => (
-            <Container key={i}>
-              <Cell
-                {..._}
-                data-id={i}
-                onDragOver={(e) => e.preventDefault()}
-                onDrop={dragDrop}
-                onDragEnd={dragEnd}
-                style={{ backgroundColor: _.owner }}
-              ></Cell>
-            </Container>
+            <Cell
+              key={i}
+              data-id={i}
+              onDragOver={(e) => e.preventDefault()}
+              onDrop={dragDrop}
+              onDragEnd={dragEnd}
+              style={{ backgroundColor: _.owner }}
+            ></Cell>
           ))}
         </Board>
-        <ScoreBlue>{parseInt(blueScore)}</ScoreBlue>
+        <P2Score>{parseInt(p2Score)}</P2Score>
         <Hand>
-          {blueHand.map((card, i) => (
-            <BlueCards
-              key={card.number}
+          {p2Hand.map((card, i) => (
+            <P2Cards
+              key={card.id}
               data-id={i}
-              draggable={isBlueTurn ? true : false}
+              draggable={isP1Turn ? false : true}
               onDragStart={dragStart}
               onDragEnd={dragEnd}
             >
-              <Card className="card blue" {...card} name={card.name} owner={card.owner}>
-                <CharImage src={`../images/cardImages/card${card.number}.png`} alt={card.name}></CharImage>
-                <Values>
-                  <Up>{card.values[0]}</Up>
-                  <Right>{card.values[1]}</Right>
-                  <Left>{card.values[2]}</Left>
-                  <Down>{card.values[3]}</Down>
-                </Values>
-              </Card>
-            </BlueCards>
+              <Card {...card} owner={card.owner}></Card>
+            </P2Cards>
           ))}
         </Hand>
-        <TurnmarkerBlue>{isBlueTurn ? "<--" : ""}</TurnmarkerBlue>
+        <P2TurnMarker>{isP1Turn ? "" : "<--"}</P2TurnMarker>
       </Table>
-      <Link to="../MatchEnd">
-        <Button label={">"} />
-      </Link>
-    </MatchLayout>
+    </DevLayout>
   );
 };
 
-const MatchLayout = styled.div`
-  width: 100vw;
+const DevLayout = styled.div`
+  width: 99vw;
   height: 100vh;
-  box-sizing: border-box;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  box-sizing: border-box;
   justify-content: space-around;
+  align-items: center;
+  background-image: url("./images/stone_table.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 const Board = styled.div`
@@ -323,7 +250,7 @@ const Board = styled.div`
 const Cell = styled.div`
   width: 10.5vw;
   height: 14.7vw;
-  border: 2px dotted black;
+  border: 2px dotted grey;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -378,72 +305,19 @@ const Table = styled.div`
   justify-content: space-around;
 `;
 
-const Card = styled.div`
-  width: 10vw;
-  height: 14vw;
-  cursor: pointer;
-  border: 2px solid black;
-  display: flex;
-  justify-content: center;
-`;
-
-const Container = styled.div``;
-
-const RedCards = styled.div`
+const P1Cards = styled.div`
   & > .card {
     background-color: red;
   }
 `;
 
-const BlueCards = styled.div`
+const P2Cards = styled.div`
   & > .card {
     background-color: blue;
   }
 `;
 
-const CharImage = styled.img`
-  width: 105%;
-  height: 105%;
-  pointer-events: none;
-  border-radius: 8px;
-  border: 2px solid black;
-  margin-top: -2.5%;
-`;
-
-const Values = styled.div`
-  color: white;
-  font-weight: bold;
-  font-size: 1.25vw;
-  pointer-events: none;
-  z-index: 99;
-  position: absolute;
-`;
-
-const Up = styled.span`
-  display: flex;
-  margin-top: 0.25vw;
-  margin-left: 7vw;
-`;
-
-const Right = styled.span`
-  display: flex;
-  margin-top: -0.75vw;
-  margin-left: 6vw;
-`;
-
-const Left = styled.span`
-  display: flex;
-  margin-top: -1.7vw;
-  margin-left: 8vw;
-`;
-
-const Down = styled.span`
-  display: flex;
-  margin-top: -0.75vw;
-  margin-left: 7vw;
-`;
-
-const TurnmarkerBlue = styled.div`
+const P2TurnMarker = styled.div`
   display: flex;
   align-self: center;
   width: 6vw;
@@ -454,7 +328,7 @@ const TurnmarkerBlue = styled.div`
   margin-top: -7vw;
 `;
 
-const TurnmarkerRed = styled.div`
+const P1TurnMarker = styled.div`
   display: flex;
   align-self: center;
   width: 6vw;
@@ -465,7 +339,7 @@ const TurnmarkerRed = styled.div`
   margin-top: -7vw;
 `;
 
-const ScoreBlue = styled.div`
+const P2Score = styled.div`
   display: flex;
   justify-content: center;
   width: 4vw;
@@ -475,7 +349,7 @@ const ScoreBlue = styled.div`
   margin-right: -10vw;
 `;
 
-const ScoreRed = styled.div`
+const P1Score = styled.div`
   display: flex;
   justify-content: center;
   width: 4vw;
@@ -483,6 +357,10 @@ const ScoreRed = styled.div`
   color: red;
   margin-right: -6vw;
   margin-left: -10vw;
+`;
+
+const ButtonBar = styled.div`
+  display: flex;
 `;
 
 export default Match;
